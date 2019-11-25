43d67a42a0d428e4933a514c9d99e4c1b51e0a66
Mohamad-Ashmar <s176492@student.dtu.dk>
2019-11-25 08:51:31 +0100

    Prompt the user to enter two rectangles

--- src/com/company/Exercise_03/_28/GeometryTwoRectangles.java
+++ src/com/company/Exercise_03/_28/GeometryTwoRectangles.java
@@ -7,12 +7,22 @@ public class GeometryTwoRectangles {
         Scanner input = new Scanner( System.in );
 
         // Rectangle number 1
-        int width1, height1, centerX1, centerY1;
+        double width1, height1, centerX1, centerY1;
 
         // Rectangle number 2
-        int width2, height2, centerX2, CenterY2;
+        double width2, height2, centerX2, centerY2;
 
         // Prompt the user to enter two rectangles
-
+        for (int i = 1; i <= 2 ; i++) {
+            System.out.print("Enter r"+i+"'s center x-, y-coordinates, width, and height: ");
+            if (i == 1){
+                width1 = input.nextDouble(); height1 = input.nextDouble();
+                centerY1 = input.nextDouble(); centerY1 = input.nextDouble();
+            }else {
+                width2 = input.nextDouble(); height2 = input.nextDouble();
+                centerY2 = input.nextDouble(); centerY2 = input.nextDouble();
+            }
+        }
+        //
     }
 }
