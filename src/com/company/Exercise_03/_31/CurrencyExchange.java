e823cd996fb80f28e10c6cf18ad34919a939b5fa
Mohamad-Ashmar <s176492@student.dtu.dk>
2019-12-07 08:53:52 +0100

    currency exchange

--- src/com/company/Exercise_03/_31/CurrencyExchange.java
+++ src/com/company/Exercise_03/_31/CurrencyExchange.java
@@ -3,12 +3,28 @@ package com.company.Exercise_03._31;
 import java.util.Scanner;
 
 
-public class CurrencyExchange {
+public class CurrencyExchange {// github and .43 not .42
     public static void main(String[] args) {
         Scanner scan = new Scanner( System.in );
 
         // Prompt the user to enter the next following
         System.out.print("Enter the exchange rate from dollars to RMB: ");
-//        int rmb = scan.
+        double rmb = scan.nextDouble();
+//        double rmb = 6.81;
+        System.out.print("Enter 0 to convert dollars to RMB and 1 vice versa: ");
+        int exchange = scan.nextInt();
+
+        switch (exchange){
+            case 0: {
+                System.out.print("Enter the dollar amount: ");
+                int amount = scan.nextInt();
+                System.out.println(amount + " is " + (amount * rmb) + " yuan");
+            }break;
+            case 1:{
+                System.out.print("Enter the RMB amount: ");
+                int amount = scan.nextInt();
+                System.out.println(amount + " is " + (int)(amount *  100.0 / rmb )/100.0 + " yuan");
+            }break;
+        }
     }
 }
