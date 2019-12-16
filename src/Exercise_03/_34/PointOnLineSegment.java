28d57ee386ceb4d07af79852f16abe6118ef5f3d
Mohamad-Ashmar <s176492@student.dtu.dk>
2019-12-16 21:50:29 +0100

    PointOnLineSegment not complete

--- src/Exercise_03/_34/PointOnLineSegment.java
+++ src/Exercise_03/_34/PointOnLineSegment.java
@@ -2,19 +2,21 @@ package Exercise_03._34;
 
 import java.util.Scanner;
 
-public class PointOnLineSegment {
+public class PointOnLineSegment {// I don't understand it
     public static void main(String[] args) {
         Scanner scan = new Scanner( System.in );
 
+        // Prompt the user to enter the three points for p0, p1 and p2.
         System.out.println("Enter three points for p0, p1 and p2: ");
-        double x0 = scan.nextDouble(), y0 = scan.nextDouble();
-        double x1 = scan.nextDouble(), y1 = scan.nextDouble();
-        double x2 = scan.nextDouble(), y2 = scan.nextDouble();
+        double x0 = scan.nextDouble(); double y0 = scan.nextDouble();
+        double x1 = scan.nextDouble(); double y1 = scan.nextDouble();
+        double x2 = scan.nextDouble(); double y2 = scan.nextDouble();
 
-        double status = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
-        System.out.println(status);
+        // Calculate point in on line segment
+        double position = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
+        System.out.println(position);
 
-//        System.out.println((status <= ));
+//        System.out.println((position <= ));
 
 
     }
