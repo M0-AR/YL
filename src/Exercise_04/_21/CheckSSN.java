d4f4af96b40c3c14059b6351c4c0b64563dfd6fa
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-01-18 10:18:29 +0100

    21 and 22

--- src/Exercise_04/_21/CheckSSN.java
+++ src/Exercise_04/_21/CheckSSN.java
@@ -1,20 +1,31 @@
 package Exercise_04._21;
-
+/*
+(Check SSN) Write a program that prompts the user to enter a Social Security
+number in the format DDD-DD-DDDD, where D is a digit. Your program should
+check whether the input is valid.
+*/
 import java.util.Scanner;
 
 public class CheckSSN {
-    public static void main(String[] args) {// how about input letter
+    public static void main(String[] args) {
         Scanner scanner = new Scanner( System.in );
 
         // Prompt the user to enter
-        System.out.print("Enter a SSN: ");
+        System.out.print( "Enter a SSN: " );
         String ssn = scanner.nextLine();
 
         // Display result
-        if (ssn.length() == 11 && ssn.charAt( 3 ) == '-' && ssn.charAt( 6 ) == '-'){
-            System.out.println(ssn + " is a valid social security number");
-        }else {
-            System.out.println(ssn + " is an invalid social security number");
-        }
+        boolean isValid = (ssn.length() == 11)
+                && (Character.isDigit( 0 ))
+                && (Character.isDigit( 1 ))
+                && (Character.isDigit( 2 ))
+                && ssn.charAt( 3 ) == '-'
+                && (Character.isDigit( 4 ))
+                && (Character.isDigit( 5 ))
+                && ssn.charAt( 6 ) == '-'
+                && (Character.isDigit( 7 )) && (Character.isDigit( 8 ))
+                && (Character.isDigit( 9 )) && (Character.isDigit( 10 ));
+
+        System.out.println(ssn + " is " + ((isValid) ? "a valid " : "an invalid ") + "social security number");
     }
 }
--- src/Exercise_04/_22/CheckSubstring.java
+++ src/Exercise_04/_22/CheckSubstring.java
@@ -1,9 +1,12 @@
 package Exercise_04._22;
-
+/*
+(Check substring) Write a program that prompts the user to enter two strings and
+reports whether the second string is a substring of the first string.
+*/
 import java.util.Scanner;
 
 public class CheckSubstring {
-    public static void main(String[] args) {// g and y
+    public static void main(String[] args) {
         Scanner scanner = new Scanner( System.in );
 
         // Prompt the user to enter string1 and string2
@@ -12,11 +15,15 @@ public class CheckSubstring {
         System.out.print("Enter string s2: ");
         String s2 = scanner.nextLine();
 
-        if (s1.indexOf( s2 ) > -1){
+        // Display result1
+        if (s1.indexOf( s2 ) != -1){
             System.out.println(s2 + " is a substring of " + s1);
         }else {
             System.out.println(s2 + " is not a substring of " + s1);
         }
+
+        // Display result2
+        System.out.println(s2 + ((s1.contains( s2 )) ? " is ": " is not ")+ " a substring of " + s1);
 
 
 
