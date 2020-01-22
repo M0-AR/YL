8ca5fef8313d0adc347ebb0cdb712423bd74d864
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-01-22 09:03:35 +0100

    MonetaryUnits 04-26 substring and indexOf

--- src/Exercise_04/_26/MonetaryUnits.java
+++ src/Exercise_04/_26/MonetaryUnits.java
@@ -3,18 +3,39 @@ package Exercise_04._26;
 import java.util.Scanner;
 
 public class MonetaryUnits {
-    public static void main(String[] args) { // con page 64
+    public static void main(String[] args) { // g
         Scanner input = new Scanner( System.in );
 
         // Receive the amount
-        System.out.print("Enter an amount in double, for example 11.56: ");
-        double amount = input.nextDouble();
+        System.out.print( "Enter an amount: " );
+        String amountString = "11.56";// = input.nextLine(); TODO
+
+        int decimalPosition = amountString.indexOf( '.' );
+
+        int amount; // amount in cents
+
+        if (decimalPosition == -1) {
+            amount = Integer.parseInt( amountString );
+        } else {
+            String fractionPart = amountString.substring( decimalPosition + 1 ); // +1 because to not take . with
+
+            if (fractionPart.length() >= 2) {
+                fractionPart = fractionPart.substring( 0, 2 );
+            } else if (fractionPart.length() == 1) {
+                fractionPart += "0";
+            } else {
+                fractionPart += "00";
+            }
+
+            amount = Integer.parseInt(
+                    amountString.substring( 0, decimalPosition ) + fractionPart );
+        }
 
         // Convert the amount into cents
-        int remainingAmount = (int)(amount * 100);
+        int remainingAmount = amount;
 
         // Find the number of dollars in the remaining amount
-        int numberOfDollars = remainingAmount / 100;
+        int numberOfOneDollars = remainingAmount / 100;
         remainingAmount %= 100;
 
         // Find the number of dimes in the remaining amount
@@ -29,6 +50,15 @@ public class MonetaryUnits {
         int numberOfNickels = remainingAmount / 5;
         remainingAmount %= 5;
 
+        // Find the number of pennies in the remaining amount
+        int numberOfPennies = remainingAmount;
 
+        // Display results
+        System.out.println( "Your amount " + amountString + " consists of \n" +
+                "\t" + numberOfOneDollars + " dollars\n" +
+                "\t" + numberOfQuarters + " quarters\n" +
+                "\t" + numberOfDimes + " dimes\n" +
+                "\t" + numberOfNickels + " nickels\n" +
+                "\t" + numberOfPennies + " pennies" );
     }
 }
