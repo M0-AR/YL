8e9955291c291b4bd4d236141d784e1f3369b8f6
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-06-06 06:47:17 +0200

    - Array  1- Review: EightQueens
     2- More assigment:

--- src/Exercise_07/_36/EightQueens.java
+++ src/Exercise_07/_36/EightQueens.java
@@ -1,6 +1,20 @@
 package Exercise_07._36;
-
-public class EightQueens { // debugger
+/**************************************************************************
+ * (Game: Eight Queens) The classic Eight Queens puzzle is to place       *
+ * eight queens on a chessboard such that no two queens can attack each   *
+ * other (i.e., no two queens are on the same row, same column, or same   *
+ * diagonal). There are many possible solutions. Write a program that     *
+ * displays one such solution. A sample output is shown below:            *
+ * |Q| | | | | | | |                                                      *
+ * | | | | |Q| | | |                                                      *
+ * | | | | | | | |Q|                                                      *
+ * | | | | | |Q| | |                                                      *
+ * | | |Q| | | | | |                                                      *
+ * | | | | | | |Q| |                                                      *
+ * | |Q| | | | | | |                                                      *
+ * | | | |Q| | | | |                                                      *
+ * ***********************************************************************/
+public class EightQueens {
     public static void main(String[] args) {
         // Queen positions
         int[] queens = new int[8]; // queens are placed at (i, queens[i])
