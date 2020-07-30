f7d38c01831e1cdb2aee8a0ae61374a9584eb2b4
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-07-30 18:36:02 +0200

    - Array  1- Review:  ExploreMatrix
     2- More assigment:

--- src/Exercise_08/_14/ExploreMatrix.java
+++ src/Exercise_08/_14/ExploreMatrix.java
@@ -2,7 +2,7 @@ package Exercise_08._14;
 
 import java.util.Scanner;
 
-public class ExploreMatrix {
+public class ExploreMatrix { // github and yl
     public static void main(String[] args) {
         // Create a scanner
         Scanner input = new Scanner( System.in );
@@ -19,28 +19,95 @@ public class ExploreMatrix {
         for (int i = 0; i < zeroOrOne.length; i++)
             for (int j = 0; j < zeroOrOne[i].length; j++)
                 zeroOrOne[i][j] = (int)(Math.random() * 2);
+            
 
+        
         // Print result
+        for (int[] ints : zeroOrOne) {
+            for (int anInt : ints) {
+                System.out.print( anInt + " " );
+            }
+            System.out.println();
+        }
+
+        boolean noSameNumber = true;
         // Row checking
-        for (int i = 0; i < zeroOrOne.length; i++) {
-            if (checkRow(zeroOrOne[i]))
+        for (int i = 0; i < zeroOrOne.length; i++)
+            if (checkRow(zeroOrOne[i])) {
                 System.out.println("All " + (zeroOrOne[i][0] == 0 ? "0":"1")
-                                                             + "s on row " + i);
-            else
-                System.out.println("No same numbers on a row");
+                        + "s on row " + i);
+                noSameNumber = false;
+            }
+
+        if (noSameNumber)
+            System.out.println("No same numbers on a row");
+
 
 
         // Column checking
-            for (int j = 0; j < zeroOrOne.length; j++) {
-
+        noSameNumber = true;
+        for (int i = 0; i < zeroOrOne.length; i++) {
+            if (checkColumn(zeroOrOne, i)) {
+                System.out.println("All " + (zeroOrOne[i][0] == 0 ? "0":"1")
+                        + "s on column " + i);
+                noSameNumber = false;
             }
         }
 
-        checkColumn(zeroOrOne);
-        checkMajorDiagonal(zeroOrOne);
-        checkSubDiagonal(zeroOrOne);
+        if (noSameNumber)
+            System.out.println("No same numbers on a column");
+
+
+
+        // MajorDiagonal checking
+        if (checkMajorDiagonal(zeroOrOne))
+            System.out.println("All " + (zeroOrOne[0][0] == 0 ? "0":"1")
+                    + "s on the major diagonal ");
+        else
+            System.out.println("No same numbers on the major diagonal");
+
+
+
+        // Sub-diagonal checking
+        if (checkSubDiagonal(zeroOrOne))
+            System.out.println("All " + (zeroOrOne[0][3] == 0 ? "0":"1")
+                    + "s on the sub-diagonal ");
+        else
+            System.out.println("No same numbers on the sub-diagonal");
+
 
     }
 
+    /** Check if there is a row has same number*/
+    public static boolean checkRow(int[] a) {
+        for (int i = 0; i < a.length-1; i++)
+            if (a[i] != a[i+1])
+                return false;
+        return true;
+    }
+
+    /** Check if there is a column has same number*/
+    public static boolean checkColumn(int[][] a, int column) {
+        for (int i = 0; i < a.length-1; i++)
+            if (a[i][column] != a[i+1][column])
+                return false;
+        return true;
+    }
+
+    /** Check if a major diagonal has same number */
+    public static boolean checkMajorDiagonal(int[][] a) {
+        for (int i = 0; i < a.length-1; i++)
+            if (a[i][i] != a[i+1][i+1])
+                return false;
+        return true;
+    }
+
+    /** Check if a sub diagonal has same number */
+    public static boolean checkSubDiagonal(int[][] a) {
+        for (int i = 0, j = a.length-1; i < a.length-1; i++, j--)
+            if (a[i][j] != a[i+1][j-1])
+                return false;
+        return true;
+    }
 
 }
