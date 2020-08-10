52c092015d7ea4f500186962cd9afe3aa6d7d4d6
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-08-10 07:01:17 +0200

    - Array  1- Review:  SortTwoDimensionalArray
     2- More assigment: FinancialTsunami

--- src/Exercise_08/_16/SortTwoDimensionalArray.java
+++ src/Exercise_08/_16/SortTwoDimensionalArray.java
@@ -1,15 +1,34 @@
 package Exercise_08._16;
-
+/*********************************************************************************
+ * (Sort two-dimensional array) Write a method to sort a two-dimensional array    *
+ * using the following header:                                                    *
+ *                                                                                *
+ * public static void sort(int m[][])                                             *
+ *                                                                                *
+ * The method performs a primary sort on rows and a secondary sort on iumns.    *
+ * For example, the following array                                               *
+ *                                                                                *
+ * {{4, 2},{1, 7},{4, 5},{1, 2},{1, 1},{4, 1}}                                    *
+ *                                                                                *
+ * will be sorted to                                                              *
+ *                                                                                *
+ * {{1, 1},{1, 2},{1, 7},{4, 1},{4, 2},{4, 5}}.                                   *
+ *********************************************************************************/
 public class SortTwoDimensionalArray {
+    /** Main method */
     public static void main(String[] args) {
+        // Initialize array
         int[][] m = {{4, 2}, {1, 7}, {4, 5},
                         {1, 2}, {1, 1}, {4, 1}};
+        // Sort array
         sort( m );
 
         // Print result
         printTwoDimensionalArray( m );
     }
 
+
+    /** Sort an 2D array primary on rows and secondary on columns */
     public static void sort(int m[][]) {
         for (int i = 0; i < m.length; i++) {
             int currentMin = m[i][0];
@@ -37,6 +56,7 @@ public class SortTwoDimensionalArray {
         }
     }
 
+    /** Display 2D array with length in every row is two */
     public static void printTwoDimensionalArray(int[][] m) {
         for (int i = 0; i < m.length; i++) {
             System.out.println(m[i][0] + ", " + m[i][1]);
---
+++ src/Exercise_08/_17/FinancialTsunami.java
@@ -0,0 +1,23 @@
+package Exercise_08._17;
+
+import java.util.Scanner;
+
+/**
+ *
+ * **/
+public class FinancialTsunami { // Need to understand the problem
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        // Create a scanner
+        Scanner input = new Scanner( System.in );
+
+        // Prompt the user to enter numbers of banks and limit
+        System.out.print( "Enter numbers of banks and limit: " );
+        int numbersOfBanks = input.nextInt();
+        int minimumTotalAssets = input.nextInt();
+
+
+    }
+}
