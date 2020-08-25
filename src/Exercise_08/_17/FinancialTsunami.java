017bef15862a34a09e9be304e9d07a15093412e6
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-08-25 19:05:26 +0200

    - Array  1- Review:
     2- More assigment: FinancialTsunami

--- src/Exercise_08/_17/FinancialTsunami.java
+++ src/Exercise_08/_17/FinancialTsunami.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 
 public class FinancialTsunami { // check how to print plural and singular and 3 first then 1
-    /**
-     * Main method
-     */
     public static void main(String[] args) {
         // Create a scanner
         Scanner input = new Scanner( System.in );
@@ -19,10 +16,9 @@ public class FinancialTsunami { // check how to print plural and singular and 3 
         numbersOfBanks = 5;
         minimumTotalAssets = 201;
 
-        // The first number in the line is the bank’s balance.
-        // Second number indicates the number of banks that
-        // borrowed money from the bank.
         double[] bankBalances = {25, 125,175,75,181};
+        // First number indicates the number of banks that
+        // borrowed money from the bank.
         double[][] borrowers = {{2, 1, 100.5, 4, 320.5},
                                 {2, 2, 40, 3, 85},
                                 {2, 0, 125, 3, 75},
@@ -59,18 +55,6 @@ public class FinancialTsunami { // check how to print plural and singular and 3 
         return unsafeBank;
     }
 
-    public static double[][] setUnsafeBankToZero(double[][] borrowers, int unsafeBankNumber) {
-        double[][] b = borrowers;
-        for (int i = 0; i < b.length; i++) {
-            for (int j = 1; j < b[i].length; j+=2) {
-                if (b[i][j] == unsafeBankNumber)
-                    b[i][j+1] = 0;
-            }
-        }
-        return b;
-    }
-
-
     public static double getTotalAssetsOfBank(double bankBalance, double[][] borrowers, int indexOfBank) {
         final int INDEX_OF_BORROWERS_BANK_LOAN = 2;
 
@@ -84,4 +68,18 @@ public class FinancialTsunami { // check how to print plural and singular and 3 
 
         return totalAssetsOfBank;
     }
+
+    public static double[][] setUnsafeBankToZero(double[][] borrowers, int unsafeBankNumber) {
+        double[][] b = borrowers;
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 1; j < b[i].length; j+=2) {
+                if (b[i][j] == unsafeBankNumber)
+                    b[i][j+1] = 0;
+            }
+        }
+        return b;
+    }
+
+
+
 }
