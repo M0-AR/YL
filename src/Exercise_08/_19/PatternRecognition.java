4008a93e7817e59bafec4dcffb5c72b71b931b41
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-09-30 09:16:40 +0200

    - Array  1- Review: PatternRecognition
     2- More assigment: PatternRecognition

--- src/Exercise_08/_19/PatternRecognition.java
+++ src/Exercise_08/_19/PatternRecognition.java
@@ -1,13 +1,23 @@
 package Exercise_08._19;
 
-public class PatternRecognition { // Diagonally
+public class PatternRecognition { // Diagonally checking back
     public static void main(String[] args) {
+        int[][] values = {
+                {0, 1, 0, 3, 1, 6, 1},
+                {0, 1, 6, 8, 6, 0, 1},
+                {5, 5, 2, 1, 6, 2, 9},
+                {6, 5, 6, 6, 1, 9, 1},
+                {1, 5, 6, 1, 4, 0, 7},
+                {3, 6, 3, 3, 4, 0, 7}
+        };
+
+        System.out.println(isConsecutiveFour( values ));
 
     }
 
 
     public static boolean isConsecutiveFour(int[][] values) {
-        int fourConsecutiveCount = 0;
+        int fourConsecutiveCount = 1;
 
         // Horizontally checking
         for (int row = 0; row < values.length; row++) {
@@ -19,7 +29,7 @@ public class PatternRecognition { // Diagonally
             if (fourConsecutiveCount == 4) {
                 return true;
             }
-            fourConsecutiveCount = 0;
+            fourConsecutiveCount = 1;
 
         }
 
@@ -27,27 +37,51 @@ public class PatternRecognition { // Diagonally
 
 
         // Vertically checking
-        for (int column = 0; column < values.length; column++) {
-            for (int row = 0; row < values[row].length; row++) {
-                if (values[column][row] == values[column][row+1]) {
+        for (int column = 0; column < values[0].length; column++) {
+            for (int row = 0; row < values.length - 1; row++) {
+                if (values[row][column] == values[row + 1][column]) {
                     fourConsecutiveCount++;
+                } else {
+                    fourConsecutiveCount = 1;
                 }
             }
 
             if (fourConsecutiveCount == 4) {
                 return true;
             }
-            fourConsecutiveCount = 0;
+            fourConsecutiveCount = 1;
         }
 
 
 
         // Diagonally checking
-        for (int row = 4; row < values.length; row++) {
-            for (int column = 0; column < values[row].length; column++) {
+        int row, column;
+        for (int i = 3; i < values.length; i++) {
+           row = i;
+            for (int j = 0; j < values[i].length - 3; j++) {
+                column = j;
+                for (; row > 0; row--) {
+                    for (; column < i; column++) {
+                        if (values[row][column] == values[row-1][column+1]) {
+                            fourConsecutiveCount++;
+                        }
+                    }
 
+                    if (fourConsecutiveCount == 4) {
+                        return true;
+                    }
+                    fourConsecutiveCount = 0;
+                }
             }
+
+
         }
+
+
+
+
+
+        return false;
 
     }
 }
