05dd05bdf810facf1cece918b506f7ae36ed3c24
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-01-27 08:59:24 +0100

    PolygonSubAreas: write getIntersectingPoint method

--- src/Exercise_08/_33/PolygonSubAreas.java
+++ src/Exercise_08/_33/PolygonSubAreas.java
@@ -27,6 +27,35 @@ public class PolygonSubAreas {
         return pointsInput;
     }
 
+    private static double[] getIntersectingPoint(double[][] points) {
+        double[][] p1 = findTwoFurthesPoints(points);
+        double[][] p2 = exclude(points, p1);
+
+        // y1 - y2
+        double a = p1[0][1] - p1[1][1];
+        // -(x1 - x2)
+        double b = -(p1[0][0] - p1[1][0]);
+        // y3 - y4
+        double c = p2[0][1] - p2[1][1];
+        // -(x3 - x4)
+        double d = -(p2[0][0] - p2[1][0]);
+        // (y1 - y2) * x1 - (x1 - x2) *y1
+        double e = ((p1[0][1] - p1[1][1]) * p1[0][0]) -
+                        ((p1[0][0] - p1[1][0]) * p1[0][1]);
+        // (y3 - y4) * x3 - (x3 - x4) *x3
+        double f = ((p2[0][1] - p2[1][1]) * p2[0][0]) -
+                        ((p2[0][0] - p2[1][0]) * p2[0][1]);
+
+        double ad_Minus_bc = a * d - b * c;
+
+        if (ad_Minus_bc == 0) return null;
+        double[] point = new double[2];
+        point[0] = (e * d - b * f) / ad_Minus_bc;
+        point[1] = (a * f - e * c) / ad_Minus_bc;
+
+        return point;
+    }
+
 
     private static double[] getFourAreasTriangles(double[][] points) {
         double[] areas = new double[4];
