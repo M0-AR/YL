7063b01ed8726319f57cfb7b031d58eafb48cca8
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-03-16 12:25:31 +0100

    Add methods to MyPoint

--- src/Exercise_10/_04/MyPoint.java
+++ src/Exercise_10/_04/MyPoint.java
@@ -5,13 +5,26 @@ public class MyPoint {
     private double y;
 
     public MyPoint() {
-        this(0,0);
     }
 
     public MyPoint(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public double distance(MyPoint secondPoint) {
+        return distance( this, secondPoint );
+    }
+
+//    public static double distance(MyPoint p1, MyPoint p2) {
+//        return p2.distance( p1 );
+//    }
+
+    public static double distance(MyPoint p1, MyPoint p2) {
+        return Math.sqrt( Math.pow((p1.x - p2.x), 2)
+                            + Math.pow((p1.y - p2.y), 2));
+    }
+
 
     public double getX() {
         return x;
@@ -21,19 +34,4 @@ public class MyPoint {
         return y;
     }
 
-    public double distance() {
-        // Todo:
-        // return Math.sqrt(  );
-        return 0;
-    }
-
-    public double distance(MyPoint myPoint) {
-        return Math.sqrt( Math.pow((this.x - myPoint.x), 2)
-                        + Math.pow((this.y - myPoint.y), 2));
-    }
-
-    public static double distance(MyPoint myPoint1, MyPoint myPoint2) {
-        return Math.sqrt( Math.pow((myPoint1.x - myPoint2.x), 2)
-                + Math.pow((myPoint1.y - myPoint2.y), 2));
-    }
 }
