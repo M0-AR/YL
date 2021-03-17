f6877834535d26854d4c1df6b7d5d180bb32b1d7
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-03-17 16:13:07 +0100

    Delete unnecessary method and make a class diagram for MyPoint class

--- src/Exercise_10/_04/MyPoint.java
+++ src/Exercise_10/_04/MyPoint.java
@@ -1,10 +1,23 @@
 package Exercise_10._04;
-
+/***********************************************
+ *             MyPoint                         *
+ *---------------------------------------------*
+ * - x: double                                 *
+ * - y: double                                 *
+ *---------------------------------------------*
+ * + MyPoint()                                 *
+ * + MyPoint(x: double, y: double)             *
+ * + getX(): double                            *
+ * + getY(): double                            *
+ * + distance(myPoint: MyPoint): double        *
+ * + distance(p1: MyPoint, p2: MyPoint): double*
+ ***********************************************/
 public class MyPoint {
     private double x;
     private double y;
 
     public MyPoint() {
+        this(0, 0);
     }
 
     public MyPoint(double x, double y) {
@@ -16,13 +29,10 @@ public class MyPoint {
         return distance( this, secondPoint );
     }
 
-//    public static double distance(MyPoint p1, MyPoint p2) {
-//        return p2.distance( p1 );
-//    }
 
     public static double distance(MyPoint p1, MyPoint p2) {
-        return Math.sqrt( Math.pow((p1.x - p2.x), 2)
-                            + Math.pow((p1.y - p2.y), 2));
+        return Math.sqrt( Math.pow((p1.x - p2.x), 2) +
+                          Math.pow((p1.y - p2.y), 2));
     }
 
 
