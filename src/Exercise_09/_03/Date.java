a30beb5527d6e0690a2f4cbcee4ccb8d8a47d5a6
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-03-23 20:45:02 +0100

    Delete the non-moral code and replace it with more nicer code

--- src/Exercise_09/_03/Date.java
+++ src/Exercise_09/_03/Date.java
@@ -1,21 +1,17 @@
 package Exercise_09._03;
-
+/*********************************************************************************
+ * (Use the Date class) Write a program that creates a Date object, sets its      *
+ * elapsedtime to 10000, 100000, 1000000, 10000000, 100000000, 1000000000,        *
+ * 10000000000, and 100000000000, and displays the date and time using the        *
+ * toString() method, respectively.                                               *
+ *********************************************************************************/
 public class Date {
     public static void main(String[] args) {
-        final int NUMBER_OF_SET_ELAPSED_TIME = 8;
+        java.util.Date date = new java.util.Date();
 
-        long elapsedTime = 100000;
-        for (int i = 0; i < NUMBER_OF_SET_ELAPSED_TIME; i++) {
-            java.util.Date date = new java.util.Date(elapsedTime);
-            System.out.println(date.toString());
-            elapsedTime = addZeroToTheRightOfNumber(elapsedTime);
-            System.out.println(elapsedTime);
+        for (long i = 10000; i <= 1e11; i *= 10) {
+            date.setTime( i );
+            System.out.println( date.toString() );
         }
-    }
-
-    private static long addZeroToTheRightOfNumber(long elapsedTime) {
-        String addZeroToTheRight = String.valueOf( elapsedTime );
-        addZeroToTheRight += "0";
-        return Long.parseLong( addZeroToTheRight );
     }
 }
