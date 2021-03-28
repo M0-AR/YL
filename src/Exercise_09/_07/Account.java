26968a82f49ae4a784a675976997368700bbcafe
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-03-28 11:30:12 +0200

    Design a uml diagram Account class

---
+++ src/Exercise_09/_07/Account.java
@@ -0,0 +1,20 @@
+package Exercise_09._07;
+
+import java.util.Date;
+
+/********************************************
+ *               Account                    *
+ *------------------------------------------*
+ * -id: int                                 *
+ * -balance: double                         *
+ * -annualInterestRate: double              *
+ * -dateCreated: Date                       *
+ *------------------------------------------*
+ * +getMonthlyInterestRate(): double        *
+ * +getMonthlyInterest(): double            *
+ * +withdraw(amount: double): void          *
+ * +deposit(amount: double); void           *
+ *------------------------------------------*/
+
+public class Account {
+}
