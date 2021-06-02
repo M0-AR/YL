0fb359293e9d994b2c5c8e30d4d5de6e5d66a25c
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-06-02 10:23:20 +0200

    Print with two decimal number.

--- src/Exercise_09/_09/TestRegularPolygon.java
+++ src/Exercise_09/_09/TestRegularPolygon.java
@@ -4,14 +4,14 @@ public class TestRegularPolygon {
     public static void main(String[] args) {
         RegularPolygon regularPolygon1 = new RegularPolygon( 6, 4 );
         System.out.println("Display Perimeter and Area of first Regular Polygon:");
-        System.out.println("Perimeter: " + regularPolygon1.getPerimeter());
-        System.out.println("Area: " + regularPolygon1.getArea());
+        System.out.printf("Perimeter: %.2f\n", regularPolygon1.getPerimeter());
+        System.out.printf("Area: %.2f", regularPolygon1.getArea());
 
         System.out.println();
 
         RegularPolygon regularPolygon2 = new RegularPolygon(10, 4, 5.8, 7.8);
         System.out.println("Display Perimeter and Area of second Regular Polygon:");
-        System.out.println("Perimeter: " + regularPolygon2.getPerimeter());
-        System.out.println("Area: " + regularPolygon2.getArea());
+        System.out.printf("Perimeter: %.2f\n", regularPolygon2.getPerimeter());
+        System.out.printf("Area: %.2f", regularPolygon2.getArea());
     }
 }
