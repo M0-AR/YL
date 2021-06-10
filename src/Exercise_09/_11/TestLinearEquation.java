ee905e554b1227884155b1be2388580763d8ce11
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-06-10 09:07:38 +0200

    Add text question

--- src/Exercise_09/_11/TestLinearEquation.java
+++ src/Exercise_09/_11/TestLinearEquation.java
@@ -1,5 +1,22 @@
 package Exercise_09._11;
-
+/*********************************************************************************
+ * (Algebra: 2 * 2 linear equations) Design a class named LinearEquation for a    *
+ * 2 * 2 system of linear equations:                                              *
+ *                                                                                *
+ *  ax + by = e, cx + dy = f; x = (ed - bf) / (ad - bc) y = (af - ec) / (ad - bc) *
+ *                                                                                *
+ * The class contains:                                                            *
+ * ■ Private data fields a, b, c, d, e, and f.                                    *
+ * ■ A constructor with the arguments for a, b, c, d, e, and f.                   *
+ * ■ Six getter methods for a, b, c, d, e, and f.                                 *
+ * ■ A method named isSolvable() that returns true if ad - bc is not 0.           *
+ * ■ Methods getX() and getY() that return the solution for the equation.         *
+ *                                                                                *
+ * Draw the UML diagram for the class and then implement the class. Write a test  *
+ * program that prompts the user to enter a, b, c, d, e, and f and displays the   *
+ * result. If ad - bc is 0, report that “The equation has no solution.” See       *
+ * Programming Exercise 3.3 for sample runs.                                      *
+ *********************************************************************************/
 import java.util.Scanner;
 
 public class TestLinearEquation {
