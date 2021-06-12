bde4b912e25209bfce201d0d6694d3796563542e
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-06-12 16:08:52 +0200

    Fill Location class with the right attributes and method

--- src/Exercise_09/_13/Location.java
+++ src/Exercise_09/_13/Location.java
@@ -9,5 +9,28 @@ package Exercise_09._13;
  * + locateLarges(a: double[][]): Location *
  *******************************************/
 public class Location {
+    private int[][] locations;
+    private double maxValue;
 
+    private Location(int[][] locations, double maxValue) {
+        this.locations = locations;
+        this.maxValue = maxValue;
+    }
+
+    public static Location locateLargest(double[][] a) {
+       int[][] maxLocation = new int[1][2];
+       double maxValue = Double.MIN_VALUE;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] > maxValue) {
+                    maxValue = a[i][j];
+                    maxLocation[0][0] = i;
+                    maxLocation[0][1] = j;
+                }
+            }
+        }
+
+        return new Location(maxLocation, maxValue);
+    }
 }
