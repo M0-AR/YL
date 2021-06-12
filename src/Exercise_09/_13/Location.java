407a3e74b38434e07ae5f2fcd203cd8034e4525a
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-06-12 16:01:50 +0200

    Draw a UML diagram for Location class

---
+++ src/Exercise_09/_13/Location.java
@@ -0,0 +1,13 @@
+package Exercise_09._13;
+            // UML diagram
+/*******************************************
+ *                Location                 *
+ *-----------------------------------------*
+ * + locations: int[][]                    *
+ * + maxValue: double                      *
+ * ----------------------------------------*
+ * + locateLarges(a: double[][]): Location *
+ *******************************************/
+public class Location {
+
+}
