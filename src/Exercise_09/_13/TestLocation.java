f792daf25755c16d550c582f7b672ad6712f7cd2
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-06-13 14:56:20 +0200

    Add comments to the code

--- src/Exercise_09/_13/TestLocation.java
+++ src/Exercise_09/_13/TestLocation.java
@@ -6,6 +6,7 @@ public class TestLocation {
     public static void main(String[] args) {
         Scanner input = new Scanner( System.in );
 
+        // Prompt the user to enter a two-dimensional array
         System.out.print("Enter the number of rows and columns in the array: ");
         int row = input.nextInt();
         int column = input.nextInt();
@@ -18,8 +19,10 @@ public class TestLocation {
             }
         }
 
+        // Get location
         Location location = Location.locateLargest(locations);
 
+        // Display the location of the largest element in the array
         System.out.println("The location of the largest element is " +
                 location.maxValue + " at (" + location.locations[0][0] +
                                     ", " + location.locations[0][1] + ")");
