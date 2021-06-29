3584be02d38512049aca1472f268a9803db4fbbc
m0-ar <s176492@student.dtu.dk>
2021-06-29 20:03:55 +0200

    Modify Course class

--- src/Exercise_10/_09/Course.java
+++ src/Exercise_10/_09/Course.java
@@ -9,11 +9,6 @@ public class Course {
         this.courseName = courseName;
     }
 
-    public void addStudent(String student) {
-        students[numberOfStudents] = student;
-        numberOfStudents++;
-    }
-
     public String[] getStudents() {
         return students;
     }
@@ -26,17 +21,52 @@ public class Course {
         return courseName;
     }
 
+    public void addStudent(String student) {
+        if (students.length == numberOfStudents) {
+            String[] s = new String[students.length * 2];
+
+            System.arraycopy(students, 0, s, 0, students.length);
+            students = s;
+        }
+
+        students[numberOfStudents] = student;
+        numberOfStudents++;
+    }
+
     public void dropStudent(String student) {
-        String[] newStudents = new String[100];
+        int index = findStudent(student);
+        if (index != -1) {
+            dropStudent(index);
+        } else {
+            System.out.println(student +
+                " is not in the course: " + courseName);
+        }
+    }
+
+    private void dropStudent(int index) {
+        String[] s = new String[students.length - 1];
 
         for (int i = 0, j = 0; i < numberOfStudents; i++) {
-            if (!students[i].equalsIgnoreCase(student)) {
-                newStudents[j++] = students[i];
+            if (i != index) {
+                s[j++] = students[i];
             }
         }
 
-        students = newStudents;
+        students = s;
         numberOfStudents--;
+    }
+
+    private int findStudent(String student) {
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equalsIgnoreCase(student))
+                return i;
+        }
+        return -1;
+    }
+
+    public void clear() {
+        students = new String[100];
+        numberOfStudents = 0;
     }
 
 }
