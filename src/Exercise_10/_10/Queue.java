7625ccce87d398330b1d6ae1a590029b9f2fa91f
m0-ar <s176492@student.dtu.dk>
2021-06-30 18:31:30 +0200

    Create Queue class and UML diagram

---
+++ src/Exercise_10/_10/Queue.java
@@ -0,0 +1,56 @@
+package Exercise_10._10;
+        // UML diagram
+/****************************
+ *          Queue           *
+ ****************************
+ * -elements: int[]         *
+ * -size: int               *
+ *--------------------------*
+ * +enqueue(v: int): void   *
+ * +dequeue(): int          *
+ * +empty(): boolean        *
+ * +getSize(): int          *
+ ****************************/
+public class Queue {
+    private int[] elements;
+    private int size;
+
+    public Queue() {
+        elements = new int[8];
+        size = 0;
+    }
+
+    public void enqueue(int v) {
+        if (size == elements.length) {
+            int[] e = new int[elements.length * 2];
+
+            System.arraycopy(elements, 0, e, 0, elements.length);
+
+            elements = e;
+        }
+        elements[size] = v;
+        size++;
+    }
+
+    public int dequeue() {
+        int firstElement = elements[0];
+
+        int[] e = new int[elements.length - 1];
+
+        for (int i = 1; i < elements.length - 1; i++) {
+            e[i - 1] = elements[i];
+        }
+
+        elements = e;
+
+       return firstElement;
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size + 1;
+    }
+}
