f7f4acf0ea16dbc7f72eff5a65c27d78972945b3
m0-ar <s176492@student.dtu.dk>
2021-07-02 00:49:21 +0200

    Modify TestQueue

--- src/Exercise_10/_10/TestQueue.java
+++ src/Exercise_10/_10/TestQueue.java
@@ -5,13 +5,12 @@ public class TestQueue {
         Queue queue = new Queue();
 
         // Adds 20 numbers from 1 to 20 into the queue
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 20; i++)
             queue.enqueue(i);
-        }
+
 
         // Removes the numbers from queue and displays them
-        for (int i = 0; i < 20; i++) {
-            System.out.print(queue.dequeue() + ((i != 19) ? ", " : ""));
-        }
+        while (!queue.empty())
+            System.out.print(queue.dequeue() + " ");
     }
 }
