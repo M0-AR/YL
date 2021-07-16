def49bce2c84559e528d2b4c22ee4f723d972917
m0-ar <s176492@student.dtu.dk>
2021-07-16 08:17:14 +0200

    Create MyRectangle2D class

---
+++ src/Exercise_10/_13/MyRectangle2D.java
@@ -0,0 +1,74 @@
+package Exercise_10._13;
+
+public class MyRectangle2D {
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    public MyRectangle2D() {
+        this.x = 0;
+        this.y = 0;
+        this.width = 1;
+        this.height = 1;
+    }
+
+    public MyRectangle2D(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double area() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return -1;
+    }
+
+    public boolean contains(double x, double y) {
+        return false;
+    }
+
+    public boolean contains(MyRectangle2D r) {
+        return false;
+    }
+
+    public boolean overlaps(MyRectangle2D r) {
+        return false;
+    }
+}
