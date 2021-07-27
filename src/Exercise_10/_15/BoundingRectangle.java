9fd4c4c7365e76bcf689534f21a611353c4b46f6
m0-ar <s176492@student.dtu.dk>
2021-07-27 09:12:37 +0200

    Try to solve Bounding Rectangle problem

---
+++ src/Exercise_10/_15/BoundingRectangle.java
@@ -0,0 +1,14 @@
+package Exercise_10._15;
+
+import Exercise_10._13.MyRectangle2D;
+
+public class BoundingRectangle {
+    public static void main(String[] args) {
+        MyRectangle2D myRectangle2D = new MyRectangle2D();
+    }
+
+    public static MyRectangle2D getRectangle(double[][] points) {
+        return new MyRectangle2D();
+    }
+
+}
