861d6ba8a578d07099e2b09ea9ee4ce6b71b037f
m0-ar <s176492@student.dtu.dk>
2021-07-31 13:35:27 +0200

    Add question's text

--- src/Exercise_10/_16/DivisibleBy2Or3.java
+++ src/Exercise_10/_16/DivisibleBy2Or3.java
@@ -1,7 +1,9 @@
 package Exercise_10._16;
-
+/***********************************************************************************
+ * (Divisible by 2 or 3) Find the first 10 numbers with 50 decimal digits that are *
+ * divisible by 2 or 3.                                                            *
+ **********************************************************************************/
 import java.math.BigInteger;
-
 public class DivisibleBy2Or3 {
     public static void main(String[] args) {
         // BigNum represents Long.MAX_VALUE
@@ -12,7 +14,6 @@ public class DivisibleBy2Or3 {
 
         int count = 1;
         while (count <= 10) {
-            // Todo
             if (bigNum.remainder(new BigInteger("2")).equals(BigInteger.ZERO) ||
                 bigNum.divide(new BigInteger("3")).equals(BigInteger.ZERO)) {
                 System.out.println(bigNum);
