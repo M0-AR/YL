eb048c25d2dbe9a5ce4801b72709b416e4b7a1bd
m0-ar <s176492@student.dtu.dk>
2021-08-01 21:31:31 +0200

    SquareNumbers done

--- src/Exercise_10/_17/SquareNumbers.java
+++ src/Exercise_10/_17/SquareNumbers.java
@@ -1,13 +1,18 @@
 package Exercise_10._17;
-
+/***********************************************************************************
+ * (Square numbers) Find the first 10 square numbers that are greater than Long.   *
+ * MAX_VALUE. A square number is a number in the form of n2. For example, 4, 9,    *
+ * and 16 are square numbers. Find an efficient approach to run your program fast. *                                                           *
+ ***********************************************************************************/
 import java.math.BigInteger;
-// Todo: check if the solution is correct.
+
 public class SquareNumbers {
     public static void main(String[] args) {
-        System.out.println(Long.MAX_VALUE);
-        BigInteger bigInteger = new BigInteger(Long.MAX_VALUE + "");
+        long longMax = (long) Math.sqrt(Long.MAX_VALUE);
+
+        BigInteger bigInteger;
         for (int i = 0; i < 10; i++) {
-            bigInteger = bigInteger.add(BigInteger.valueOf(Long.parseLong(i + "")));
+            bigInteger = new BigInteger(longMax + i + 1 + "");
             bigInteger = bigInteger.multiply(bigInteger);
             System.out.println(bigInteger);
         }
