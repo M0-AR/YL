26a8d064c2a6eaf2876790f817a1948e283d2423
m0-ar <s176492@student.dtu.dk>
2021-08-04 23:48:55 +0200

    The right solution for LargePrimeNumbers

--- src/Exercise_10/_18/LargePrimeNumbers.java
+++ src/Exercise_10/_18/LargePrimeNumbers.java
@@ -7,6 +7,8 @@ import java.math.BigInteger;
 
 public class LargePrimeNumbers {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
         // BigNum represents Long.MAX_VALUE
         BigInteger bigNum = new BigInteger(Long.MAX_VALUE + "");
         // BigNum is now larger than Long.MAX_VALUE
@@ -14,13 +16,36 @@ public class LargePrimeNumbers {
 
         int count = 1;
         while (count <= 5) {
-            if (!bigNum.remainder(new BigInteger("2")).equals(BigInteger.ZERO) &&
-                    !bigNum.divide(new BigInteger("3")).equals(BigInteger.ZERO)) {
+            if (isPrime(bigNum)) {
                 System.out.println(bigNum);
                 count++;
             }
 
             bigNum = bigNum.add(BigInteger.ONE);
         }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time spent is " +
+                (endTime - startTime) + " milliseconds");
     }
+
+    public static boolean isPrime(BigInteger num) {
+        if (num.compareTo(BigInteger.ONE) == 0 ||
+                num.compareTo(new BigInteger("2")) == 0) {
+            return true;
+        }
+
+        BigInteger halfNum = num.divide(new BigInteger("2"));
+
+        for (BigInteger divisor = new BigInteger("2");
+                divisor.compareTo(halfNum) <= 0;
+                divisor = divisor.add(BigInteger.ONE)) {
+            if (num.remainder(divisor).equals(BigInteger.ZERO)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
