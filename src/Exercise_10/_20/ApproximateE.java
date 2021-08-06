f8840cef7bcd736b66fb47c8be42bb29286a315c
m0-ar <s176492@student.dtu.dk>
2021-08-06 21:06:43 +0200

    Question's text.

--- src/Exercise_10/_20/ApproximateE.java
+++ src/Exercise_10/_20/ApproximateE.java
@@ -1,5 +1,14 @@
 package Exercise_10._20;
-
+/*********************************************************************************
+ * (Approximate e) Programming Exercise 5.26 approximates e using the following   *
+ * series:                                                                        *
+ *                                                                                *
+ *       e = 1 + (1 / 1!) + (1 / 2!) + (1 / 3!) + (1 / 4!) + ... + (1 / i!)       *
+ *                                                                                *
+ * In order to get better precision, use BigDecimal with 25 digits of precision   *
+ * in the computation. Write a program that displays the e value for              *
+ * i = 100, 200, . . . , and 1000.                                                *
+ *********************************************************************************/
 import java.math.BigDecimal;
 
 public class ApproximateE {
