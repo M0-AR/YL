979d9745325e1fda6fcb48c12c1dd883d4b77b93
m0-ar <s176492@student.dtu.dk>
2021-08-13 21:45:26 +0200

    Question for MyString1

--- src/Exercise_10/_22/TestMyString1.java
+++ src/Exercise_10/_22/TestMyString1.java
@@ -1,4 +1,17 @@
 package Exercise_10._22;
+/*********************************************************************************
+ * (Implement the String class) The String class is provided in the Java library.*
+ * Provide your own implementation for the following methods (name the new       *
+ * class MyString1):                                                             *
+ *                                                                               *
+ * public MyString1(char[] chars);                                               *
+ * public char charAt(int index);                                                *
+ * public int length();                                                          *
+ * public MyString1 substring(int begin, int end);                               *
+ * public MyString1 toLowerCase();                                               *
+ * public boolean equals(MyString1 s);                                           *
+ * public static MyString1 valueOf(int i);                                       *
+ ********************************************************************************/
 
 public class TestMyString1 {
     public static void main(String[] args) {
