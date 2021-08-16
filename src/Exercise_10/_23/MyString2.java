18d6aff66f09a952032cc595c1aa7e431cc02bd6
m0-ar <s176492@student.dtu.dk>
2021-08-16 22:53:46 +0200

    80% of this class is done.

--- src/Exercise_10/_23/MyString2.java
+++ src/Exercise_10/_23/MyString2.java
@@ -4,26 +4,62 @@ public class MyString2 {
     private String str;
 
     public MyString2(String str) {
-        this.str = str;
+        this.str = "";
+        for (int i = 0; i < str.length(); i++){
+            this.str += str.charAt(i) + "";
+        }
     }
 
+    /** Return an integer greater than 0, equal to 0,
+     *  or less than 0 to indicate whether this string
+     *  is greater than, equal to, or less than this str.*/
     public int compare(String s) {
-        return str.compareTo(s);
+        int index = 0;
+
+        if (this.str.length() < s.length()) {
+            index = -1;
+        }
+        if (s.length() < this.str.length()) {
+            index = 1;
+        }
+
+        for (int i = 0; i < this.str.length(); i++) {
+            if (this.str.charAt(i) < s.charAt(i))
+                index = -(i + 1);
+            if (this.str.charAt(i) > s.charAt(i))
+                index = i + 1;
+        }
+        return index;
     }
 
+    /** Return a new myString2 object that start
+     *  with a specified begin.*/
     public MyString2 substring(int begin) {
-        return new MyString2(str.substring(begin));
+        StringBuilder sub = new StringBuilder();
+        for (int i = begin; i < this.str.length(); i++) {
+            sub.append(this.str.charAt(i));
+        }
+        return new MyString2(sub.toString());
     }
 
+    // Todo: continue with more comments to the rest of the methods. 
     public MyString2 toUpperCase() {
-        return new MyString2(str.toUpperCase());
+        String s = "";
+        for (int i = 0; i < this.str.length(); i++) {
+            s += Character.toUpperCase(this.str.charAt(i));
+        }
+        return new MyString2(s);
     }
 
     public char[] toChars() {
-        return str.toCharArray();
+        char[] temp = new char[this.str.length()];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = this.str.charAt(i);
+        }
+        return temp;
     }
 
     public static MyString2 valueOf(boolean b) {
-        return null;
+        return b ? new MyString2("true") : new MyString2("false");
     }
 }
