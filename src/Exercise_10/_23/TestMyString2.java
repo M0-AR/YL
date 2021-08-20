6709181f2cb7ba346aa82b77653bb1514f899150
m0-ar <s176492@student.dtu.dk>
2021-08-20 19:33:44 +0200

    Add MyString2 question.

--- src/Exercise_10/_23/TestMyString2.java
+++ src/Exercise_10/_23/TestMyString2.java
@@ -1,5 +1,15 @@
 package Exercise_10._23;
-
+/**********************************************************************************
+ * (Implement the String class) The String class is provided in the Java library. *
+ * Provide your own implementation for the following methods (name the new        *
+ * class MyString2):                                                              *
+ * public MyString2(String s);                                                    *
+ * public int compare(String s);                                                  *
+ * public MyString2 substring(int begin);                                         *
+ * public MyString2 toUpperCase();                                                *
+ * public char[] toChars();                                                       *
+ * public static MyString2 valueOf(boolean b);                                    *
+ *********************************************************************************/
 public class TestMyString2 {
     public static void main(String[] args) {
         // Create two MyString2 objects
