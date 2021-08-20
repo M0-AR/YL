0f5b80b4f952af84be0b3af5d6b2d8b94dbd5223
m0-ar <s176492@student.dtu.dk>
2021-08-20 19:31:51 +0200

    Make a test class for MyString2.

---
+++ src/Exercise_10/_23/TestMyString2.java
@@ -0,0 +1,38 @@
+package Exercise_10._23;
+
+public class TestMyString2 {
+    public static void main(String[] args) {
+        // Create two MyString2 objects
+        MyString2 str1 = new MyString2("test1");
+        MyString2 str2 = new MyString2("test2");
+
+        // Compare
+        System.out.println("Compare str1 to \"test1\": ");
+        System.out.println(str1.compare("test1"));
+
+        // Substring
+        System.out.println("\nDisplay str2 at index 3: ");
+        print(str2.substring(3));
+
+        // ToUpperCase
+        System.out.println("\nConvert str2 to upper case:");
+        print(str2.toUpperCase());
+
+        // ValueOf
+        System.out.println("\nDisplay the value true as a MyString2 object: ");
+        print(MyString2.valueOf(true));
+    }
+
+    /** Display a MyString2 object */
+    public static void print(MyString2 str) {
+        print(str.toChars());
+    }
+
+    /** Display an array of character */
+    private static void print(char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            System.out.print(chars[i]);
+        }
+        System.out.println();
+    }
+}
