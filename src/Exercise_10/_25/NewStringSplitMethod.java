1f8c0b8143a00837674b759b8ace924198e95de3
m0-ar <s176492@student.dtu.dk>
2021-08-26 11:12:40 +0200

    Solve for just if there is one char in regex.

--- src/Exercise_10/_25/NewStringSplitMethod.java
+++ src/Exercise_10/_25/NewStringSplitMethod.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 public class NewStringSplitMethod {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(split("ab#12#453", "#")));
-        System.out.println(Arrays.toString(split("a?b?gf#e", "[?#]")));
+        System.out.println(Arrays.toString(split("ab#12#453", "#")));   // ["ab", "#", "12", "#", "453"]
+        System.out.println(Arrays.toString(split("a?b?gf#e", "[?#]"))); // ["a", "?", "b", "?", "gf", "#", "e"]
     }
 
     public static String[] split(String s, String regex) {
         String[] result = new String[s.length()];
+        Arrays.fill(result, "");
 
         char[] ch = new char[regex.length()];
         for (int i = 0, j = 0; i < regex.length(); i++) {
@@ -17,13 +18,26 @@ public class NewStringSplitMethod {
             ch[j++] = regex.charAt(i);
         }
 
+        if (regex.length() == 1) {
+            ch[0] = regex.charAt(0);
+        }
 
-
-        for (int i = 0; i < s.length(); i++) {
-
+        int k = 0;
+        boolean breakAllLoops = false;
+        for (int i = 0; i < s.length(); i++) { // Iterate 'i' throw regex characters.
             for (int j = 0; j < s.length(); j++) {
-                result[i] += s.charAt(j);
+                while (!breakAllLoops) {
+                    if (s.charAt(k) == ch[i]) {
+                        result[++j] = s.charAt(k++) + "";
+                        j++;
+                        break;
+                    }
+                    result[j] += s.charAt(k++);
+                    if (k == s.length()) breakAllLoops = true;
+                }
+                if (breakAllLoops) break;
             }
+            if (breakAllLoops) break;
         }
 
         return null;
