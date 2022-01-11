6bde1115a2d3f3a2219fe13f8dfb299b0d0238c5
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-01-11 05:46:39 +0100

    SavingsAccount

---
+++ src/Exercise_11/_03/SavingsAccount.java
@@ -0,0 +1,29 @@
+package Exercise_11._03;
+/***********************************
+ *          SavingsAccount         *
+ ***********************************
+ * +SavingsAccount()               *
+ * +withdraw(amount: double): void *
+ ***********************************/
+public class SavingsAccount extends Account{
+
+    /** Construct a default CheckingAccount object */
+    public SavingsAccount() {
+        super();
+    }
+
+    /** Construct a SavingsAccount with specified id, balance */
+    public SavingsAccount(int id, int balance, int overdrawn) {
+        super(id, balance);
+    }
+
+    /** Decrease balance by amount */
+    public void withdraw(double amount) {
+        if (amount <= getBalance()) {
+            setBalance(getBalance() - amount);
+        } else {
+            System.out.println("Error! amount is overdrawn, transaction is rejected.");
+        }
+    }
+
+}
