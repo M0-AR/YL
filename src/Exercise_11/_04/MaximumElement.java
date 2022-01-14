93d4dc10e96ede33d5b22ac55f191c7659fe41c1
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-01-14 14:11:32 +0100

    Take care of list if null or empty.

--- src/Exercise_11/_04/MaximumElement.java
+++ src/Exercise_11/_04/MaximumElement.java
@@ -5,26 +5,30 @@ import java.util.Scanner;
 
 public class MaximumElement {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList();
 
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter a sequence of numbers ending with 0: ");
-        int end = -1;
-        ArrayList numbers = new ArrayList();
+        int userInput, end = -1;
 
         while (end != 0) {
-            int userInput = input.nextInt();
+            userInput = input.nextInt();
             numbers.add(userInput);
             end = userInput;
         }
 
-        int maxNumber = max(numbers);
-        System.out.println(maxNumber);
+        // Display the maximum number
+        System.out.println(max(numbers));
     }
 
     public static Integer max(ArrayList<Integer> list) {
+        if (list == null || list.size() == 0)
+            return null;
+
         int max = list.get(0);
         for (Integer integer : list) {
-            if (max < integer) max = integer;
+            if (max < integer)
+                max = integer;
         }
         return max;
     }
