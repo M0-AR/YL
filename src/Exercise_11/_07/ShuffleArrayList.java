4bd43b1614dde40f316abf0d848aeb4d1916bd42
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-01-24 22:56:30 +0100

    Refactoring, prompt the user to enter array of numbers and write comments.

--- src/Exercise_11/_07/ShuffleArrayList.java
+++ src/Exercise_11/_07/ShuffleArrayList.java
@@ -1,19 +1,36 @@
 package Exercise_11._07;
-
+/*********************************************************************************
+ * (Shuffle ArrayList) Write the following method that shuffles the elements in   *
+ * an ArrayList of integers.                                                      *
+ *                                                                                *
+ * public static void shuffle(ArrayList<Integer> list)                            *
+ *********************************************************************************/
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class ShuffleArrayList {
     public static void main(String[] args) {
-        // Initiate array of numbers
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,4,5,6));
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Create ArrayList of Integer type
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // Prompt the user to enter a list of Integer ending with 0
+        System.out.println("Enter a list of integers ending with 0: ");
+        Integer number = input.nextInt();
+
+        while (number.intValue() != 0) {
+            numbers.add(number);
+            number = input.nextInt();
+        }
 
         // Shuffle numbers
         shuffle(numbers);
 
         // Print numbers
-        for (Integer number : numbers) {
-            System.out.print(number + ", ");
+        for (Integer n : numbers) {
+            System.out.print(n + " ");
         }
 
     }
