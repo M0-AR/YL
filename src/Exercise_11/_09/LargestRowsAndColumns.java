b4532c6388d805d6a24678850c758559c4ff6845
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-02-13 21:30:41 +0100

    count 1 bit in row and display max

--- src/Exercise_11/_09/LargestRowsAndColumns.java
+++ src/Exercise_11/_09/LargestRowsAndColumns.java
@@ -18,7 +18,7 @@ public class LargestRowsAndColumns {
         }
 
 
-        ArrayList<Integer> maxRowWith1Bit = new ArrayList<>();
+        ArrayList<Integer> rowWith1Bit = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             int count1Bit = 0;
             for (int j = 0; j < n; j++) {
@@ -26,14 +26,31 @@ public class LargestRowsAndColumns {
                    count1Bit++;
                 }
             }
-            maxRowWith1Bit.add(count1Bit);
+            rowWith1Bit.add(count1Bit);
         }
 
 
-        // Find max in maxRowWith1Bit
-        // Todo
+        // Find max in rowWith1Bit
+        ArrayList<Integer> maxRowWith1Bit = new ArrayList<>();
+        for (int i = 0; i < rowWith1Bit.size(); i++) {
+            if (i == 0) maxRowWith1Bit.add(rowWith1Bit.get(0));
+            if (rowWith1Bit.get(i) > maxRowWith1Bit.get(0)) {
+                maxRowWith1Bit = new ArrayList<>();
+                maxRowWith1Bit.add(rowWith1Bit.get(i));
+            } else if (rowWith1Bit.get(i) == maxRowWith1Bit.get(0)) {
+                maxRowWith1Bit.add(rowWith1Bit.get(i));
+            }
+        }
+        /* // bug with this input
+        0111
+        0011
+        1101
+        1010
+         */
 
-
+        for (Integer integer : maxRowWith1Bit) {
+            System.out.print(integer + " ");
+        }
 
         ArrayList<Integer> maxColumnWith1Bit= new ArrayList<>();
         for (int i = 0; i < n; i++) {
