a2b8bec8d754df827e53800eaad8920074292691
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-03-05 09:26:22 +0100

    Add getIndex, fill and print functions

--- src/Exercise_11/_09/LargestRowsAndColumns.java
+++ src/Exercise_11/_09/LargestRowsAndColumns.java
@@ -74,7 +74,7 @@ public class LargestRowsAndColumns {
         }
     }
 
-
+   /** Return the maximum integer in the array */
    public static int max(int[] a) {
         int max = a[0];
         for (int i = 1; i < a.length; i++) {
@@ -84,6 +84,34 @@ public class LargestRowsAndColumns {
         return max;
    }
 
+   /** Adds the indices of x in count to the list */
+   public static void getIndex(ArrayList<Integer> list, int[] count, int x) {
+       for (int i = 0; i < count.length; i++) {
+           if (count[i] == x)
+               list.add(i);
+       }
+   }
+
+
+   /** Fill array randomly with 0s and 1s */
+   public static void fill(int[][] array) {
+       for (int i = 0; i < array.length; i++) {
+           for (int j = 0; j < array.length; j++) {
+               array[i][j] = (int)(Math.random() * 2);
+           }
+       }
+   }
+
+   /** Display matrix */
+   public static void print(int[][] array) {
+       System.out.println("The random array is");
+       for (int i = 0; i < array.length; i++) {
+           for (int j = 0; j < array.length; j++) {
+               System.out.println(array[i][j]);
+           }
+           System.out.println();
+       }
+   }
 
 }
 
