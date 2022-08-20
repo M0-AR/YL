af165f2a820c5e091f0d498ce7cd6b7f20e8a36d
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-08-20 20:40:48 +0200

    Combine two lists

---
+++ src/Exercise_11/_14/CombineTwoLists.java
@@ -0,0 +1,40 @@
+package Exercise_11._14;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CombineTwoLists {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        System.out.print("Enter five integers for list1: ");
+        for (int i = 0; i < 5; i++) {
+            list1.add(input.nextInt());
+        }
+
+        System.out.print("Enter five integers for list2: ");
+        for (int i = 0; i < 5; i++) {
+            list2.add(input.nextInt());
+        }
+
+        System.out.println("The combined list is " + union(list1, list2).toString());
+    }
+
+    public static ArrayList<Integer> union(
+            ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (Integer num : list1) {
+            list.add(num);
+        }
+
+        for (Integer num : list2) {
+            list.add(num);
+        }
+
+        return list;
+    }
+}
