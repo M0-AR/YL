affa75a00cfeeac21cab00904dbb205a8d000d7e
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-09-04 20:49:11 +0200

    Getting Input from user

--- src/Exercise_11/_19/BinPackingUsingFirstFit.java
+++ src/Exercise_11/_19/BinPackingUsingFirstFit.java
@@ -1,4 +1,24 @@
 package Exercise_11._19;
 
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class BinPackingUsingFirstFit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.ENGLISH);
+
+        ArrayList<Container> containers = new ArrayList<>();
+        ArrayList<Double> weights = new ArrayList<>();
+
+        System.out.print("Enter the number of objects: ");
+        int numberOfObject = input.nextInt();
+
+        for (int i = 1; i < numberOfObject + 1; i++) {
+            System.out.print("Enter the weight of object " + i + ": ");
+            double weight = input.nextDouble();
+            weights.add(weight);
+        }
+
+    }
 }
