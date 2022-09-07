c57a11e584f21cb3f87af124114217b0be4766d3
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-09-07 06:50:16 +0200

    Algo under development

--- src/Exercise_11/_19/Container.java
+++ src/Exercise_11/_19/Container.java
@@ -1,16 +1,31 @@
 package Exercise_11._19;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Container {
-    public final int MAX_WEIGHT = 0;
-    public static int numberOfObjects = 0;
+    public final int MAX_WEIGHT = 10;
+//    public static int numberOfObjects = 0;
     private double totalWeight = 0;
-    private ArrayList<Double> weights = new ArrayList<>();
+    private List<Double> weights = new ArrayList<>();
 
+    public void fillContainer(List<Double> weights) {
+        Double weight = weights.get(0);
 
+        while(totalWeight + weight <= MAX_WEIGHT) {
 
-    public ArrayList<Double> getWeights() {
+            this.weights.add(weight);
+            weights.remove(weight);
+            totalWeight += weight;
+
+            if (weights.isEmpty())
+                break;
+
+            weight = weights.get(0);
+        }
+    }
+
+    public List<Double> getWeights() {
         return new ArrayList<>(weights);
     }
 }
