ced771931c5a7482c5cc3a4544c7d6204d2a4035
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-09-13 08:20:41 +0200

    SortArrayList

---
+++ src/Exercise_13/_03/SortArrayList.java
@@ -0,0 +1,9 @@
+package Exercise_13._03;
+
+import java.util.ArrayList;
+
+public class SortArrayList {
+    public static void sort(ArrayList<Number> list) {
+        list.sort(null);
+    }
+}
---
+++ src/Exercise_13/_03/SortArrayListTest.java
@@ -0,0 +1,34 @@
+package Exercise_13._03;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class SortArrayListTest {
+    ArrayList<Number> numbers;
+    @Before
+    public void setUp() throws Exception {
+        numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(14);
+        numbers.add(24);
+        numbers.add(42);
+    }
+
+    @Test
+    public void testSort() {
+        ArrayList<Number> numbers = new ArrayList<>();
+        numbers.add(14);
+        numbers.add(24);
+        numbers.add(4);
+        numbers.add(42);
+        numbers.add(5);
+        SortArrayList.sort(numbers);
+        assertArrayEquals(this.numbers.toArray(), numbers.toArray());
+    }
+}
